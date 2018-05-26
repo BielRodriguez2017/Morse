db05df43183b92bdea4c45e7aa9d21376456b5fe
BielRodriguez2017 <36242017+BielRodriguez2017@users.noreply.github.com>
2018-05-26 02:22:01 +0200

    BIEL9
    Programa morse on he posat el meu nom més el meu numero del futbol.

---
+++ Morse/Morse.ino
@@ -0,0 +1,77 @@
+/******************************************************************************
+**                                                                           **
+**                              Morse                                        **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+const int pin = 8;   // pin for speaker
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  ratla();
+  punt();
+  punt();
+  punt();
+  
+  espaiL();
+  
+  punt();
+  punt();
+  
+  espaiL();
+  
+  punt();
+  ratlla();
+  punt();
+  punt();
+
+  espaiL();
+
+  ratlla();
+  ratlla();
+  ratlla();
+  ratlla();
+  punt();
+ 
+  
+  espaiP();
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
+
